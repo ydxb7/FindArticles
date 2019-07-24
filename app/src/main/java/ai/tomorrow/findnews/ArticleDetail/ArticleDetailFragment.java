613b75a860b847ad8ece273dd36adc48bb2302b4
Yini <ydxb7@mail.missouri.edu>
2019-07-24 03:46:32 -0700

    add webView

--- app/src/main/java/ai/tomorrow/findnews/ArticleDetail/ArticleDetailFragment.java
+++ app/src/main/java/ai/tomorrow/findnews/ArticleDetail/ArticleDetailFragment.java
@@ -1,10 +1,19 @@
 package ai.tomorrow.findnews.ArticleDetail;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +22,7 @@ import androidx.fragment.app.Fragment;
 
 import org.parceler.Parcels;
 
+import ai.tomorrow.findnews.MainActivity;
 import ai.tomorrow.findnews.R;
 import ai.tomorrow.findnews.database.entity.Article;
 import ai.tomorrow.findnews.databinding.FragmentArticleDetailBinding;
@@ -24,6 +34,7 @@ public class ArticleDetailFragment extends Fragment {
     private String TAG = ArticleDetailFragment.class.getSimpleName();
     private FragmentArticleDetailBinding mBinding;
     private Article mArticle;
+    private WebView myWebView;
 
     @Nullable
     @Override
@@ -33,7 +44,75 @@ public class ArticleDetailFragment extends Fragment {
 
         Log.d(TAG, "mArticle = " + mArticle);
 
+        myWebView = mBinding.webview;
+
+        setWebView();
+
 
         return mBinding.getRoot();
     }
+
+    private void setWebView() {
+        myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        // Enabling JavaScript
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setLoadsImagesAutomatically(true);
+        myWebView.setWebViewClient(new MyWebViewClient());
+        // Load the page
+        myWebView.loadUrl(mArticle.getWebUrl());
+
+        myWebView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // Check if the key event was the Back button and if there's history
+                if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == event.KEYCODE_BACK){
+                    if (myWebView.canGoBack()) {
+                        myWebView.goBack();
+                    } else {
+                        // If it wasn't the Back key or there's no web page history, bubble up to the default
+                        // system behavior (probably exit the activity)
+                        getActivity().onBackPressed();
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (Uri.parse(url).getHost().equals("https://www.nytimes.com")) {
+                // This is my website, so do not override; let my WebView load the page
+                myWebView.loadUrl(url);
+                return true;
+            }
+            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
+
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
+
+    }
+
+    public boolean canGoBack() {
+        return myWebView != null && myWebView.canGoBack();
+    }
+
+    public void goBack() {
+        if (myWebView != null) {
+            myWebView.goBack();
+        }
+    }
+
 }
--- app/src/main/java/ai/tomorrow/findnews/MainActivity.java
+++ app/src/main/java/ai/tomorrow/findnews/MainActivity.java
@@ -1,14 +1,63 @@
 package ai.tomorrow.findnews;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+
+import ai.tomorrow.findnews.ArticleDetail.ArticleDetailFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName();
+    private Fragment mCurrentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+//        NavController navController= Navigation.findNavController(MainActivity.this,R.id.nav_graph);
+//
+//        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+//            @Override
+//            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+//                Integer fragmentId = destination.getId();
+//                mCurrentFragment = getSupportFragmentManager().findFragmentById(fragmentId);
+//                Log.e(TAG, "onDestinationChanged: "+ mCurrentFragment);
+//            }
+//        });
     }
+
+//    @Override
+//    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//
+//        if(mCurrentFragment instanceof ArticleDetailFragment){
+//            if(ArticleDetailFragment.myOnKeyDown(keyCode, event))
+//                return true;
+//        }
+//        return super.onKeyDown(keyCode, event);
+//    }
+
+//    @Override
+//    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//        if (keyCode == KeyEvent.KEYCODE_MENU) {
+//            (ArticleDetailFragment) fragment.my
+//
+//
+//            ((PastEventListFragment)fragments.get(0)).myOnKeyDown(keyCode);
+//            ((EventListFragment)fragments.get(1)).myOnKeyDown(keyCode);
+//
+//            //and so on...
+//        }
+//        return super.onKeyDown(keyCode, event);
+//    }
 }
